17ef36cd86bf35a1e03f9f69957e18694ab8b9b9
Miguel Certã <112079507+mpcsource@users.noreply.github.com>
2024-12-07 15:32:40 +0000

    Minimal setup

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
+/* Imports */
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+/* Basic Setup */
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );;
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#bg'), });
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.z = 5;
+renderer.render( scene, camera );
+const controls = new OrbitControls(camera, renderer.domElement);
 
-setupCounter(document.querySelector('#counter'))
+/* Helper Stuff */
+const gridHelper = new THREE.GridHelper(200, 50)
+scene.add(gridHelper)
+
+/* Main Body */
+
+
+/* Update Function */
+let update = function() {
+
+}
+
+/* Basic Animate Function */
+let animate = function () {
+	requestAnimationFrame( animate );
+
+	update();
+
+	controls.update();
+	renderer.render( scene, camera );
+}
+
+animate()
